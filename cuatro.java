c39c76d2c3368e70172736ab47658b308be8e9ad
frafelcp <felixcastro1993@gmail.com>
2020-02-16 11:39:11 -0500

    cuatro ventana
    pasamos a ventana

--- cuatro.java
+++ cuatro.java
@@ -22,22 +22,14 @@ public class cuatro {
         int[] vNum1;
 
         //inicializacion
-//        canNum1 = Integer.parseInt(JOptionPane.showInputDialog("cantidad de numeros"));
-        canNum1 = leer.nextInt();
+        canNum1 = Integer.parseInt(JOptionPane.showInputDialog("cantidad de numeros"));
         vNum1 = new int[canNum1];
 
         //leer vector
         for (int i = 0; i < vNum1.length; i++) {
-//            vNum1[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el "
-//                    + "numero " + (i + 1)));
-            System.out.print("numero " + (i + 1) + " al vector 1:");
-            vNum1[i] = leer.nextInt();
-        }
-
-        //escribir vector
-        for (int i = 0; i < vNum1.length; i++) {
-            System.out.println("El vector 1 posicion " + (i + 1) + "es: " + vNum1[i]);
-        }
+            vNum1[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el "
+                    + "numero " + (i + 1)));
+        }        
 
         //LEER SEGUNDO VECTOR
         //variables
@@ -52,7 +44,6 @@ public class cuatro {
         for (int i = 0; i < vNum2.length; i++) {
             vNum2[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el "
                     + "numero " + (i + 1)));
-            vNum2[i] = leer.nextInt();
         }
 
         //VECTOR 3 DE SUMA DE VECTOR 1 Y 2
@@ -74,10 +65,7 @@ public class cuatro {
             suma = suma + vSum[i] + ", ";
         }
 
-        //escribir vector
-        System.out.println("El suma es " + suma);
-//        for (int i = 0; i < vSum.length; i++) {
-//            System.out.println("El vector suma posicion " + (i + 1) + "es: " + vSum[i]);
-//        }
+        //escribir vector suma
+        JOptionPane.showMessageDialog(null, "El vector suma es " + suma);        
     }
 }
